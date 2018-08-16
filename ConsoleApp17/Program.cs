f6ad7fa1298a67e209b802242a2a682f4a77b1a2
botnetorgcn <system@dotnet.org.cn>
2018-08-16 15:05:52 +0800

    添加项目文件。

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Text.StringBuilder stringBuilder = new System.Text.StringBuilder();
+            var day = 1;
+            while (day <= 31)
+            {
+                stringBuilder.AppendLine($"	  +(case  WHEN [Day{day.ToString("00")}AMType] =4 then 0.5  else 0 end)+(case  WHEN [Day{day.ToString("00")}PMType] =4  then 0.5  else 0 end)");
+                day++;
+            }
+            var text = stringBuilder.ToString();
+        }
+    }
+}
